8c90af2e26af3664bae14daef31b4250bb9e1b11
Baqir Muhammad <bmuhammnad.bscs20seecs@seecs.edu.pk>
2022-05-12 01:29:41 +0500

    First Commit

---
+++ caoproj.ino
@@ -0,0 +1,15 @@
+
+int LED_BUILTIN = 2;
+
+void setup()
+{
+pinMode (LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(LED_BUILTIN, HIGH); //turns led on
+delay(1000);// 1s delay
+digitalWrite(LED_BUILTIN, LOW);//turns led off
+delay(1000);
+}43
