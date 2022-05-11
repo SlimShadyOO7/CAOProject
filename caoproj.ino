34fb2afb188741a30883bcedcc8c95c5efe9a778
Baqir Muhammad <bmuhammnad.bscs20seecs@seecs.edu.pk>
2022-05-12 01:54:14 +0500

    Testing remote repository

--- caoproj.ino
+++ caoproj.ino
@@ -2,13 +2,13 @@ int LED_BUILTIN = 2;
 
 void setup()
 {
-pinMode (LED_BUILTIN, OUTPUT);
+    pinMode (LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-digitalWrite(LED_BUILTIN, HIGH); //turns led on
-delay(1000);// 1s delay
-digitalWrite(LED_BUILTIN, LOW);//turns led off
-delay(1000);
+    digitalWrite(LED_BUILTIN, HIGH); //turns led on
+    delay(1000);                   // 1s delay
+    digitalWrite(LED_BUILTIN, LOW);//turns led off
+    delay(1000);
 }
