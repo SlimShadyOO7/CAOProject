7cc810ff52e07a35afbdab88333ec80e40a1c9cd
Baqir Muhammad <bmuhammnad.bscs20seecs@seecs.edu.pk>
2022-05-12 02:14:30 +0500

    Made LED turn on

--- caoproj.ino
+++ caoproj.ino
@@ -9,6 +9,7 @@ void loop()
 {
     digitalWrite(LED_BUILTIN, HIGH); //turns led on
     delay(1000);                   // 1s delay
+    
     digitalWrite(LED_BUILTIN, LOW);//turns led off
     delay(1000);
 }
