af4183f2bba26c5fca97c49969b704aceb575005
baqirmuhammad <96880829+baqirmuhammad@users.noreply.github.com>
2022-05-21 01:18:23 +0500

    connected to wifi successfully

--- caoproj.ino
+++ caoproj.ino
@@ -1,10 +1,10 @@
 #include <LiquidCrystal.h>
 #include <WiFi.h>        // Include the Wi-Fi library
+
 const char* ssid     = "PTCL-BB";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "hussain12";     // The password of the Wi-Fi network
-
 // Create An LCD Object. Signals: [ RS, EN, D4, D5, D6, D7 ]
-LiquidCrystal My_LCD(13, 12, 14, 27, 26, 25);
+LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
 void setup()
 {
  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
@@ -26,15 +26,16 @@ void setup()
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
   
     // Initialize The LCD. Parameters: [ Columns, Rows ]
-  My_LCD.begin(16, 2);
-  // Clears The LCD Display
-  My_LCD.clear();
+  lcd.begin(16, 2);
 
-  // Display The First Message In Home Position (0, 0)
-  My_LCD.print("Hello World!");
-  // Set The Cursor Position To: [ Col 5, Row 1]
-  My_LCD.setCursor(5, 1);
-  // Display The Second Message In Position (5, 1)
-  My_LCD.print("Setting up...");
+  lcd.clear();  // Clears The LCD Display
+
+  lcd.setCursor(0,0);  // Display The First Message In Home Position (0, 0)
+
+  lcd.print("Covid19 Tracker");
+
+  lcd.setCursor(0,1);
+
+  lcd.print("Hand Sanitizer");
  }
 void loop(){}
