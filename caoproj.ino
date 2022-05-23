37da3cfef0782214701fcd28cae57d0c29880515
baqirmuhammad <96880829+baqirmuhammad@users.noreply.github.com>
2022-05-24 00:26:56 +0500

    made ultrasonic sensor detect hand
    prints the distance of hand from sensor on screen if it is less than 20cm

--- caoproj.ino
+++ caoproj.ino
@@ -3,32 +3,27 @@
 
 const char* ssid     = "PTCL-BB";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "hussain12";     // The password of the Wi-Fi network
+
 // Create An LCD Object. Signals: [ RS, EN, D4, D5, D6, D7 ]
 LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
+
+//defining pins for US sensor
+const int trig = 33;
+const int echo = 32;
+
+#define sound_speed 0.034 //speed of sound in cm/microsec
+long duration;//time taken by pulse to travel
+float distancecm;//distance of obstacle fro, sensor
+
 void setup()
 {
-  ///////////////////////////////////////////wifi setup///////////////////////////////////////////////
-  
   Serial.begin(115200);         // Start the Serial communication to send messages to the computer
-  delay(10);
+  delay(1000);
   Serial.println('\n');
+  pinMode(trig, OUTPUT); // Sets the trig Pin as an Output
+  pinMode(echo, INPUT); // Sets the echo Pin as an Input
   
-  WiFi.begin(ssid, password);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(500);
-    Serial.print('.');
-  }
-  Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
-  
-  
-    ///////////////////////////////////////////LCD setup///////////////////////////////////////////////
-  
+  ///////////////////////////////////////////LCD setup///////////////////////////////////////////////
   
   // Initialize The LCD. Parameters: [ Columns, Rows ]
   lcd.begin(16, 2);
@@ -42,5 +37,38 @@ void setup()
   lcd.setCursor(0,1);
 
   lcd.print("Hand Sanitizer");
+  
+  ///////////////////////////////////////////wifi setup///////////////////////////////////////////////
+    
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(500);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.println("Wifi connected!");  
+
  }
-void loop(){}
+void loop(){
+  
+  digitalWrite(trig,LOW);//clears pin
+  delayMicroseconds(2);
+  
+  // Sets the trig Pin on HIGH state for 10 micro seconds
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration=pulseIn(echo, HIGH);
+  distancecm= duration * sound_speed / 2; //distance of obstacle from sensor
+  
+  // Prints the distance in the Serial Monitor
+  if(distancecm < 20){
+    Serial.print("Distance (cm): ");
+    Serial.println(distancecm);
+  }
+  delay(1000); //1 second delay
+  }
