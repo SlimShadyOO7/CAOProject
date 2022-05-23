b701169f56c7144933ad6a8e59e8773671d221a5
baqirmuhammad <96880829+baqirmuhammad@users.noreply.github.com>
2022-05-23 20:39:32 +0500

    made lcd display data

--- caoproj.ino
+++ caoproj.ino
@@ -7,7 +7,9 @@ const char* password = "hussain12";     // The password of the Wi-Fi network
 LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
 void setup()
 {
- Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  ///////////////////////////////////////////wifi setup///////////////////////////////////////////////
+  
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
   delay(10);
   Serial.println('\n');
   
@@ -19,20 +21,23 @@ void setup()
     delay(500);
     Serial.print('.');
   }
-
   Serial.println('\n');
   Serial.println("Connection established!");  
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
   
-    // Initialize The LCD. Parameters: [ Columns, Rows ]
+  
+    ///////////////////////////////////////////LCD setup///////////////////////////////////////////////
+  
+  
+  // Initialize The LCD. Parameters: [ Columns, Rows ]
   lcd.begin(16, 2);
 
   lcd.clear();  // Clears The LCD Display
 
   lcd.setCursor(0,0);  // Display The First Message In Home Position (0, 0)
 
-  lcd.print("Covid19 Tracker");
+  lcd.print("LCD works");
 
   lcd.setCursor(0,1);
 
